b5b351b2094260b93724c1e931dbf566bef3b69b
Michael Hutchings <mhutchings0416@gmail.com>
2024-04-08 10:44:19 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WinterIntex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
