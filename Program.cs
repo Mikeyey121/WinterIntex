22725ba15066fd0c449aa309e3fca18fbe172b56
Michael Hutchings <mhutchings0416@gmail.com>
2024-04-08 13:45:17 -0600

    Updating SEcurity

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WinterIntex.Data;
 
+// Create builder variable
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
+// Adding configuration for Google login
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
 
+// Connection for sql server database 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
@@ -17,6 +26,15 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+// Configure HSTS options
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromDays(365);
+    // options.ExcludedHosts.Add("example.com"); // Use this to exclude specific hosts from HSTS
+});
 
 var app = builder.Build();
 
@@ -32,16 +50,32 @@ else
     app.UseHsts();
 }
 
+// Configure so that http request come in as https
 app.UseHttpsRedirection();
+
+// Configure for static files we want to use
 app.UseStaticFiles();
 
+// Configure for routing
 app.UseRouting();
 
+
+// Configure for Hsts middleman
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+// Configure for authorization
 app.UseAuthorization();
 
+// Configuration for mapping controller routes
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Configure for razor pages
 app.MapRazorPages();
 
+// Run that bad boy
 app.Run();
