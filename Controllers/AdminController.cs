9eb8b46c88639e84a57a83caadf4056d1286507a
jonahallen <jonahmarkallen@gmail.com>
2024-04-10 16:42:47 -0600

    admin changes

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using WinterIntex.Models;
+
+namespace WinterIntex.Controllers
+{
+    public class AdminController : Controller
+    {
+        private WinterIntexContext _context;
+        public IEnumerable<Product> Product { get; set; }
+
+        public AdminController(WinterIntexContext temp)
+        {
+            _context = temp;
+        }
+        public IActionResult Products()
+        {
+            Product = _context.Products
+                .OrderBy(x => x.Name).ToList();
+            return View(Product);
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
